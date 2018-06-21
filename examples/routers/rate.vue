4fb73cd7d7c31a6360db6046f68abf489cd4ae58
yufangyang <975057282@qq.com>
2018-06-21 14:30:44 +0800

    update

---
+++ examples/routers/rate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Rate v-model="value"></Rate>
+        <Rate allow-half v-model="valueHalf"></Rate>
+        <Rate clearable v-model="valueClear"></Rate>
+        <Rate clearable allow-half  v-model="valueClearHalf"></Rate>
+        <!--<Rate show-text v-model="valueText"></Rate>-->
+        <!--<Rate show-text allow-half v-model="valueCustomText">-->
+            <!--<span style="color: #f5a623">{{ valueCustomText }}</span>-->
+        <!--</Rate>-->
+        <!--<Rate disabled  allow-half v-model="valueDisabled"></Rate>-->
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value: 0,
+                valueHalf: 2.5,
+                valueText: 3,
+                valueCustomText: 4.0,
+                valueDisabled: 2.4,
+                valueClear: 1,
+                valueClearHalf: 1.5,
+            }
+        }
+    }
+</script>
